725dadf2b5196d9bd35c5b722a61e80337a0536b
William Phelps <williamphelps13@gmail.com>
2024-02-04 14:45:36 -0700

    Convert to Project 4 (add phone number and health check)

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 
-import models
 from database import engine
+from models import Base
 from routers import admin, auth, todos, users
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/healthy")
+def health_check():
+    return {"status": "Healthy"}
+
 
 app.include_router(auth.router)
 app.include_router(todos.router)
--- routers/users.py
+++ routers/users.py
@@ -54,3 +54,15 @@ async def change_password(
     user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
     db.add(user_model)
     db.commit()
+
+
+@router.put("/phonenumber/{phone_number}", status_code=status.HTTP_204_NO_CONTENT)
+async def change_phonenumber(
+    user: user_dependency, db: db_dependency, phone_number: str
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+    user_model.phone_number = phone_number
+    db.add(user_model)
+    db.commit()

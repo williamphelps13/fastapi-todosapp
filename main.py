4308f7b165c1613250ff21381c56cd7d509b0586
William Phelps <williamphelps13@gmail.com>
2024-02-04 16:16:07 -0700

    Add CORSMiddleware

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import engine
 from models import Base
 from routers import admin, auth, todos, users
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://williamjphelps.com/todosapp"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
